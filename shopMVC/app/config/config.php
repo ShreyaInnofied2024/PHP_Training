c54253210c143712582ff57d4795fe1095058cae
ShreyaInnofied2024 <shreya.singh@innofied.com>
2024-10-22 18:05:35 +0530

    Add files via upload

---
+++ shopMVC/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','password');
+define('DB_NAME','Product');
+
+define('APPROOT',dirname(dirname(__FILE__)));
+define('URLROOT','http://localhost/shopMVC');
+define('SITENAME', 'SHOP');
---
+++ shopMVC/app/views/product/add.php
@@ -0,0 +1,8 @@
+ <h1> ADD PRODUCT </h1>
+ <form action="<?php echo URLROOT; ?>/products/add" method="post">
+    Name: <input type="text" name="name"><br>
+    Quantity: <input type="text" name="quantity"><br>
+    Price:<input type="text" name="price"><br>
+    <input type="submit">
+</form>
+<a href='<?php echo URLROOT; ?>/products'><button>Go Back</button></a>
