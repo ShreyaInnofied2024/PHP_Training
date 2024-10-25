b87315c4369b847d45713b3531e5039c37a40ccb
ShreyaInnofied2024 <shreya.singh@innofied.com>
2024-10-25 18:02:42 +0530

    Delete shopMVC2 directory

--- shopMVC2/app/views/pages/index.php
+++
@@ -1,5 +0,0 @@
-<?php require APPROOT . '/views/inc/header.php';?>
-<h1><?php echo $data['title']; ?></h1>
-<p><?php echo $data['description']; ?></p>
-<a href='<?php echo URLROOT; ?>/products'><button>Product</button></a>
-<a href='<?php echo URLROOT; ?>/category'><button>Category</button></a>
--- shopMVC2/app/views/product/edit.php
+++
@@ -1,8 +0,0 @@
-<h1> EDIT PRODUCT </h1>
- <form action="<?php echo URLROOT; ?>/products/edit/<?php echo $data['id'];?>" method="post">
-    Name: <input type="text" name="name" value='<?php echo $data['name'];?>'><br>
-    Quantity: <input type="text" name="quantity" value='<?php echo $data['quantity'];?>'><br>
-    Price:<input type="text" name="price" value='<?php echo $data['price'];?>'><br>
-    <input type="submit">
-</form>
-<a href='<?php echo URLROOT; ?>/products'><button>Go Back</button></a>
